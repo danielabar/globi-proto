cc52b7d1697090e889ec2391fcea38db415afd84
Daniela Baron <baron.daniela@gmail.com>
2015-02-14 21:19:36 -0500

    show common name

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,10 +22,13 @@
 
   $scope.taxonSelected = function(item) {
     images.get({taxon: item}).$promise.then(function(response) {
-      $scope.taxon.commonName = response.commonName;
-      $scope.taxon.thumbnailURL = response.thumbnailURL;
-      $scope.taxon.imageURL = response.imageURL;
-      $scope.taxon.infoURL = response.infoURL;
+      $scope.taxon = {
+        scientificName: response.scientificName,
+        commonName: response.commonName,
+        thumbnailURL: response.thumbnailURL,
+        imageURL: response.imageURL,
+        infoURL: response.infoURL
+      };
       $rootScope.$emit('taxonEvent', $scope.taxon);
       $rootScope.$broadcast('taxonEvent', $scope.taxon);
     }, function(err) {
