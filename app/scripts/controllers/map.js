d415c00d1dad0347b662c451ac6c6ac4a241baec
Daniela Baron <baron.daniela@gmail.com>
2015-03-14 08:31:35 -0400

    map marker

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -9,14 +9,21 @@
  */
 angular.module('globiProtoApp')
   .controller('MapCtrl', function ($scope) {
+
     $scope.center = {
         lat: 45,
         lng: 18,
         zoom: 8
     };
-    // angular.extend($scope, {
-    //   defaults: {
-    //     // scrollWheelZoom: false
-    //   }
-    // });
+
+    $scope.markers = {
+      osloMarker: {
+          lat: 45,
+          lng: 18,
+          // message: "I want to travel here!",
+          focus: true,
+          draggable: false
+      }
+    }
+
   });
