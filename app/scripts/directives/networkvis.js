4ab84abcc236b0688f628e3b1bc862996e2414e5
Daniela Baron <baron.daniela@gmail.com>
2015-03-22 07:42:16 -0400

    make graph layout better

--- app/scripts/directives/networkvis.js
+++ app/scripts/directives/networkvis.js
@@ -11,6 +11,11 @@ angular.module('globiProtoApp')
         // Constants
         var width = 960;
         var height = 500;
+        var color = d3.scale.category20();
+        var force = d3.layout.force()
+          .charge(-120)
+          .linkDistance(30)
+          .size([width, height]);
 
         // Init the vis
         var svg = d3.select(element[0]).append('svg')
@@ -25,43 +30,38 @@ angular.module('globiProtoApp')
           // Clear out the old elements
           svg.selectAll('*').remove();
 
-          var force = d3.layout.force()
-            .charge(-120)
-            .linkDistance(30)
-            .size([width, height]);
-
+          // Force layout
           force
             .nodes(newVal.nodes)
             .links(newVal.links)
             .start();
 
           var link = svg.selectAll('.link')
-            .data(newVal.links)
+              .data(newVal.links)
             .enter().append('line')
-            .attr('class', 'link')
-            .style('stroke-width', function(d) { return Math.sqrt(d.weight); });
+              .attr('class', 'link')
+              .style('stroke-width', function(d) { return Math.sqrt(d.value); });
 
           var node = svg.selectAll('.node')
-            .data(newVal.nodes)
-            .enter().append('g')
-            .attr('class', 'node')
-            .call(force.drag);
+              .data(newVal.nodes)
+            .enter().append('circle')
+              .attr('class', 'node')
+              .attr('r', 5)
+              .style('fill', function(d) { return color(d.group); })
+              .call(force.drag);
 
-          node.append('circle')
-            .attr('r','5');
-
-          node.append('text')
-            .attr('dx', 12)
-            .attr('dy', '.35em')
-            .text(function(d) { return d.name; });
+          node.append('title')
+              .text(function(d) { return d.name; });
 
           force.on('tick', function() {
             link.attr('x1', function(d) { return d.source.x; })
                 .attr('y1', function(d) { return d.source.y; })
                 .attr('x2', function(d) { return d.target.x; })
                 .attr('y2', function(d) { return d.target.y; });
-            node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
-          });
+
+            node.attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; });
+          }); //force.on(tick)
         });//$scope.$watch
 
       }//link
