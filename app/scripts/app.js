d1efd88cdcbcd90ea35516cf7a818ed2636472f1
Daniela Baron <baron.daniela@gmail.com>
2015-03-29 13:07:03 -0400

    add ngprogress for global http loading indicator

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,7 @@ angular
   .module('globiProtoApp', [
     'ui.router',
     'ngResource',
+    'ngProgress',
     'ngAnimate',
     'ngAria',
     'ngCookies',
@@ -20,7 +21,7 @@ angular
     'ui.bootstrap',
     'leaflet-directive'
   ])
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
     $stateProvider
       .state('main', {
         url: '/main?name&interaction',
@@ -88,4 +89,46 @@ angular
         }
       });
     $urlRouterProvider.otherwise('/main');
+
+    // Connect all HTTP events to the $rootScope bus, so that we can connect them to ngProgress in run() function
+    $httpProvider.interceptors.push(function($q, $rootScope){
+      return {
+        'request': function(config) {
+          $rootScope.$emit('start request');
+          return config;
+        },
+        'requestError': function(rejection) {
+          $rootScope.$emit('end request');
+          return $q.reject(rejection);
+        },
+        'response': function(response) {
+          $rootScope.$emit('end request');
+          return response;
+        },
+        'responseError': function(rejection) {
+          $rootScope.$emit('end request');
+          return $q.reject(rejection);
+        }
+      };
+    });//$httpProvider.interceptors
+
+  });// config
+
+/**
+ * Main entry point for application
+ */
+angular.module('globiProtoApp').run(function(
+  $rootScope,
+  ngProgress) {
+
+  // Hook up starting and ending of HTTP requests to ngProgress, for pretty progress bar
+  $rootScope.$on('end request', function(){
+    ngProgress.complete();
   });
+
+  $rootScope.$on('start request', function(){
+    ngProgress.reset();
+    ngProgress.start();
+  });
+
+});//run
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/leaflet/dist/leaflet-src.js',
       'bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js',
       'bower_components/d3/d3.js',
+      'bower_components/ngprogress/build/ngProgress.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
