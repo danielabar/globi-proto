b641c12bd3225a224668b724b8cd0ffc4d3a45a9
Daniela Baron <baron.daniela@gmail.com>
2015-04-19 10:18:33 -0400

    #88 more work on custom map directive

--- app/scripts/directives/interactionmap.js
+++ app/scripts/directives/interactionmap.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // http://leafletjs.com/examples/quick-start.html
+// http://leaflet-extras.github.io/leaflet-providers/preview/index.html
 
 /**
  * @ngdoc directive
@@ -23,7 +24,6 @@ angular.module('globiProtoApp')
         element.height(columnGraphValues.height);
 
         // Initialize
-        // var map = L.map('interactionMap').setView([27.3649, -82.623643], 3);
         var map = L.map('interactionMap', {
           center: [27.3649, -82.623643],
           zoom: 3,
@@ -36,10 +36,22 @@ angular.module('globiProtoApp')
             maxZoom: 10
         }).addTo(map);
 
-        // Watch for new markers to be added
+        // Watch for new markers to be added (TODO remove others?)
         scope.$watch('observations', function(newObservations) {
-          console.log('map directive detected new observatiosn to add: ');
+          console.log('map directive detected new observations to add: ');
           console.dir(newObservations);
+          if (newObservations) {
+            Object.keys(newObservations).forEach(function(obs) {
+              var markerLocation = new L.LatLng(newObservations[obs].lat, newObservations[obs].lng);
+              var marker = new L.Marker(markerLocation);
+              // marker.bindPopup('<b>Hello world!</b><br />I am a popup.');
+              marker.bindPopup(newObservations[obs].message);
+              // L.marker([newObservations[obs].lat, newObservations[obs].lng]).addTo(map);
+              marker.addTo(map);
+            });
+          } else {
+            // TODO hide map? or some message like no geo data available...
+          }
         });
       }
     };
